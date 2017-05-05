2af91a65b47f27e083218b83c5e9b7385299e094
Brian Doig <brian.doig@me.com>
2017-05-05 08:02:56 -0700

    Initial Commit

---
+++ RXTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RXTest
+//
+//  Created by Brian Doig on 5/5/17.
+//  Copyright © 2017 Brian Doig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
